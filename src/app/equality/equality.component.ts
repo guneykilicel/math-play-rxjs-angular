52def10ad9420482f7cd3ab12a2f5742de9b51a6
guneykilicel <kilicelguney@gmail.com>
2023-07-11 16:59:05 +0300

    form arka planı

--- src/app/equality/equality.component.ts
+++ src/app/equality/equality.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-equality',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./equality.component.css']
 })
 export class EqualityComponent {
+
+  mathForm = new FormGroup(
+    {
+      firstNumber: new FormControl(this.generateNumber()),
+      secondNumber:new FormControl(this.generateNumber()),
+      answer: new FormControl('')
+    }
+  )
+
+  get firstNumber() {
+    return this.mathForm.value.firstNumber;
+  }
+
+  get secondNumber() {
+    return this.mathForm.value.secondNumber;
+  }
+
+
+  generateNumber() {
+    return Math.floor(Math.random()*10);
+  }
 
 }
